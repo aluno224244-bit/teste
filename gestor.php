f9ae32faf971fbd6a01555c227bfbe08b36c70b1
aluno224050-netizen <aluno224050@epad.edu.pt>
2026-05-22 14:54:29 +0100

    Create gestor.php

---
+++ gestor.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+    <style>
+        body.{
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Users</h1>
+    <div> 
+    <input type="text" name="user" placeholder="Adicionar utilizador">
+    <input type="email" name="alterar_nome" value="Email">
+    <input type="password" name="alterar_nome" value="Palavra-Passe">
+    <button type="submit" name="addicionar">Adicionar</button>
+    <div>
+        <input type="id">
+        <button>Remover</button>
+    </div>
+    </div>
+
+    <h1>Produtos</h1>
+    <div> 
+    <input type="text" name="produto" placeholder="Adicionar Produto">
+    <input type="number" name="preco" >
+    <button type="submit" name="addicionar">Adicionar</button>
+    <div>
+        <input type="id">
+        <button>Remover</button>
+    </div>
+    </div>
+</body>
+</html>
